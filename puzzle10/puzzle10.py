c80d89cccdf0405c826ee07a92f73166b4c30a2a
Mike Mudge <mike.mudge@gmail.com>
2018-01-12 19:12:22 +1300

    add puzzle 10 part 2 and puzzle 14,

--- puzzle10/puzzle10.py
+++ puzzle10/puzzle10.py
@@ -5,79 +5,69 @@ def main():
         lines = f.readlines()
 
         parts = map(int, lines[0].split(','))
-        print parts
-        values = range(256)
-        # values = range(5)
-        # parts = [3, 4, 1, 5]
 
-        currentIdx = 0
-        skip = 0
-        for p in parts:
-            # if p == 0 or p == 1:
-            #     continue
-            print values
-            print skip, currentIdx, p
+        (_, _, values) = knot_hash(parts)
 
-            values = rotate(values, currentIdx)
-            # reverse the first p numbers.
-            # print values[:p]
-            values[:p] = values[:p][::-1]
-            # rotate back to 0 based.
-            values = rotate(values, -currentIdx)
-
-            currentIdx = (currentIdx + p + skip) % len(values)
-            skip += 1
-
-        print 'Part 1', values
         print 'Part 1', values[0] * values[1]
         # 60270 was too high.
         # 2352 was too low.
         # 9288 is wrong.
         # 16256 is wrong.
+        # 4114 is right. 121 * 34
+
+        # Part 2, read line as ascii bytes instead
+        print lines[0]
+
+        # for testing
+        print full_knot_hash('')
+        print full_knot_hash('AoC 2017')
+        print full_knot_hash('1,2,3')
+        print full_knot_hash('1,2,4')
+
+        print full_knot_hash(lines[0])
+
+def full_knot_hash(value):
+
+    parts = [ord(c) for c in value]
+
+    # Just because the problem says to add these.
+    parts += [17, 31, 73, 47, 23]
+
+    skip = 0
+    currentIdx = 0
+    values = None
+    for i in range(64):
+        (currentIdx, skip, values) = knot_hash(parts, skip=skip, currentIdx=currentIdx, values=values)
+
+    dense_hash = []
+    for i in range(16):
+        dense = 0
+        for ii in range(16):
+            dense ^= values[i * 16 + ii]
+        dense_hash.append(dense)
+
+    result = ''.join(format(d, '02x') for d in dense_hash)
+    return result
+
+def knot_hash(parts, currentIdx=0, skip=0, values=None):
+    if not values:
+        values = range(256)
+
+    for p in parts:
+        values = rotate(values, currentIdx)
+        # reverse the first p numbers.
+        # print values[:p]
+        values[:p] = values[:p][::-1]
+        # rotate back to 0 based.
+        values = rotate(values, -currentIdx)
+
+        currentIdx = (currentIdx + p + skip) % len(values)
+        skip += 1
+
+    return currentIdx, skip, values
 
 def rotate(l, n):
     return l[n:] + l[:n]
-
-
-# def old_way():
-#     if currentIdx + p > 255:
-#         # TODO figure out the shorter length???
-#         dis_from_end = 256 - currentIdx
-#         x = currentIdx
-#         y = currentIdx + p - 256
-#         print x, '->', y, dis_from_end
-
-#         if dis_from_end > y:
-#             # More at end than start
-#             # x: x + y: 256/0: y
-
-#             # reverse and save the first part to put at the start.
-#             temp1 = values[x:x + y][::-1]
-
-#             # reverse the last part and put it at the start.
-#             values[x:x + y] = values[:y][::-1]
-
-#             # reverse the middle.
-#             values[dis_from_end + y:] = values[dis_from_end + y:][::-1]
-
-#             values[:y] = temp1
-#         else:
-#             # More at the start than at the end.
-#             # x: 256/0: dis_from_end: y
-
-#             # reverse and save the first part to put at the start.
-#             temp1 = values[x:][::-1]
-
-#             # reverse the last part and put it at the start.
-#             values[x:] = values[dis_from_end:y][::-1]
-
-#             # reverse the middle.
-#             values[:dis_from_end] = values[:dis_from_end][::-1]
-
-#             values[dis_from_end:y] = temp1
-#     else:
-#         # Normal reverse.
-#         values[currentIdx:currentIdx + p] = values[currentIdx:currentIdx + p][::-1]
 
 
 main()
--- puzzle14/puzzle14.py
+++ puzzle14/puzzle14.py
@@ -3,12 +3,111 @@ def main():
 
     key_input = 'jxqlasbh'
 
+    used = 0
+    grid = []
     for i in range(128):
-        create_hash('%s-%d' % (key_input, i))
+        row = []
+        grid.append(row)
+        hex_value = create_hash('%s-%d' % (key_input, i))
+        for c in hex_value:
+            v = int(c, 16)
+            row.append(v & 8 == 8)
+            row.append(v & 4 == 4)
+            row.append(v & 2 == 2)
+            row.append(v & 1 == 1)
+            used += bin(int(c, 16)).count("1")
+
+    # Print as a #. map?
+    print 'Part 1:', used
+
+    visited = [[None] * 128 for i in range(128)]
+
+    x = 0
+    y = 0
+    if visited[y][x]:
+        print 'hi'
+
+    group_count = 0
+    for y, row in enumerate(grid):
+        print y
+        for x, v in enumerate(row):
+            if not visited[y][x] and v:
+                # Start a new group.
+                group_count += 1
+                # Find neighbours
+                broadcast(grid, visited, group_count, x, y)
+
+    print 'Part 2:', group_count
+    # 1235 is too high
+
+    for row in visited:
+        print ''.join([((str(v % 10) if v > 1230 else '*') if v else '.') for v in row])
+
+
+def broadcast(grid, visited, group, x, y):
+    if x < 0 or y < 0 or x >= 128 or y >= 128:
+        # Out of bounds of grid.
+        return
+    if visited[y][x]:
+        return
+    if not grid[y][x]:
+        # Not a used square, so can't be part of a region.
+        return
+
+    # Mark yourself as visited, and track which group you are in.
+    visited[y][x] = group
+
+    # Now visit neighbours and update them.
+    broadcast(grid, visited, group, x + 1, y)
+    broadcast(grid, visited, group, x - 1, y)
+    broadcast(grid, visited, group, x, y + 1)
+    broadcast(grid, visited, group, x, y - 1)
 
 def create_hash(value):
-    # Needs solution to part 2 of p10
-    return value
+    return full_knot_hash(value)
+
+def full_knot_hash(value):
+
+    parts = [ord(c) for c in value]
+
+    # Just because the problem says to add these.
+    parts += [17, 31, 73, 47, 23]
+
+    skip = 0
+    currentIdx = 0
+    values = None
+    for i in range(64):
+        (currentIdx, skip, values) = knot_hash(parts, skip=skip, currentIdx=currentIdx, values=values)
+
+    dense_hash = []
+    for i in range(16):
+        dense = 0
+        for ii in range(16):
+            dense ^= values[i * 16 + ii]
+        dense_hash.append(dense)
+
+    result = ''.join(format(d, '02x') for d in dense_hash)
+    return result
+
+def knot_hash(parts, currentIdx=0, skip=0, values=None):
+    if not values:
+        values = range(256)
+
+    for p in parts:
+        values = rotate(values, currentIdx)
+        # reverse the first p numbers.
+        # print values[:p]
+        values[:p] = values[:p][::-1]
+        # rotate back to 0 based.
+        values = rotate(values, -currentIdx)
+
+        currentIdx = (currentIdx + p + skip) % len(values)
+        skip += 1
+
+    return currentIdx, skip, values
+
+def rotate(l, n):
+    return l[n:] + l[:n]
 
 
 main()
